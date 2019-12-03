e65a428dddaff8b08874bfaf8bbd56db8e130eda
Harumichi Yokoyama <h-yokoyama@jf.jp.nec.com>
2019-12-03 15:04:20 +0900

    script to install python modules

---
+++ python/install_modules.sh
@@ -0,0 +1,9 @@
+pip install -r requirements.txt
+export WELD_HOME=$(cd .. && pwd)
+
+cd pyweld;  python setup.py develop; cd ..
+
+cd grizzly; python setup.py develop; cd ..
+
+#cd numpy; python setup.py develop; cd ..
+cd numpy-0.1.0; python setup.py install; cd ..
